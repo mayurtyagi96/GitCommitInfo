6e2ef1179d5bf3c6dadee0e2abe466d2d6f53071
Mayur <mayur.kant.96@gmail.com>
2022-12-04 11:11:04 +0530

    Initial Commit

---
+++ GitCommitInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitCommitInfo
+//
+//  Created by Mayur Kant Tyagi on 04/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
